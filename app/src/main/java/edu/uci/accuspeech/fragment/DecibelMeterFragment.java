bad385bc734a1861dd769c6af4eea03c230ee67d
pashish215 <pashish215@yahoo.com>
2015-02-27 18:03:51 -0800

    Fixed many errors with Decibel Meter. Still need to implement a cleaner way of turning it on and off. The commented code will force it to run for 120 seconds then stop, but it freezes the rest of the app while it runs.

--- app/src/main/java/edu/uci/accuspeech/fragment/DecibelMeterFragment.java
+++ app/src/main/java/edu/uci/accuspeech/fragment/DecibelMeterFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,49 +17,63 @@ import edu.uci.accuspeech.R;
 
 public class DecibelMeterFragment extends Fragment {
 
-//    public class SoundMeter {
-//        static final private double EMA_FILTER = 0.6;
-//        private MediaRecorder mRecorder = null;
-//        private double mEMA = 0.0;
-//        public void start() throws IOException{
-//            if (mRecorder == null){
-//                mRecorder = new MediaRecorder();
-//                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//                mRecorder.prepare();
-//                mRecorder.start();
-//                mEMA = 0.0;
-//            }
-//        }
-//        public void stop(){
-//            if (mRecorder != null){
-//                mRecorder.stop();
-//                mRecorder.release();
-//                mRecorder = null;
-//            }
-//        }
-//        public double getAmplitude(){
-//            if (mRecorder != null)
-//            {return (mRecorder.getMaxAmplitude()/2700.0);}
-//            else
-//            {return 0;}
-//        }
-//        public double getAmplitudeEMA(){
-//            double amp = getAmplitude();
-//            mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
-//            return mEMA;
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        View rootView = inflater.inflate(R.layout.decibel_meter, container, false);
-//        SoundMeter meter = new SoundMeter();
-//        while (true){
+    public class SoundMeter {
+        static final private double EMA_FILTER = 0.6;
+        private MediaRecorder mRecorder = null;
+        private double mEMA = 0.0;
+        public void start(){
+            System.out.println("Hello1");
+            if (mRecorder == null){
+                mRecorder = new MediaRecorder();
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mRecorder.setOutputFile("/dev/null");
+                try {
+                    mRecorder.prepare();
+                } catch (IOException e) {
+                    System.out.println("Prepare Error");
+                }
+                mRecorder.start();
+                mEMA = 0.0;
+            }
+        }
+        public void stop(){
+            if (mRecorder != null){
+                mRecorder.stop();
+                mRecorder.release();
+                mRecorder = null;
+            }
+        }
+        public double getAmplitude(){
+            if (mRecorder != null)
+            {return (mRecorder.getMaxAmplitude());}
+            else
+            {return 0;}
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.decibel_meter, container, false);
+        SoundMeter meter = new SoundMeter();
+        meter.start();
+        int refresh_count = 0;
+
+//          while (refresh_count < 1200 ){
 //            double db = meter.getAmplitude();
-//            String sound = Double.toString(db);
-//            Log.d("Decibel Level:",sound);
+//            refresh_count += 1;
+//            try {
+//                Thread.sleep(33);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            String decibel = new Double(db).toString();
+//            System.out.println(decibel);
+
 //        }
-//
-//    }
+        meter.stop();
+        return rootView;
+    }
 }
